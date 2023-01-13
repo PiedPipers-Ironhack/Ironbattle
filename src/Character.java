95130afa711ef60763bb1e41602a59f0ccee8d1e
Dina El Badri <dinaelbadri94@gmail.com>
2023-01-13 16:08:51 +0100

    all classes

--- src/Character.java
+++ src/Character.java
@@ -1,2 +1,43 @@
 public class Character {
+    private String id;
+    private String name;
+    private int hp;
+    private boolean isAlive = true;
+
+    public Character(String name, int hp) {
+        setName(name);
+        setHp(hp);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
 }
--- src/Warrior.java
+++ src/Warrior.java
@@ -1,2 +1,32 @@
-public class Warrior {
+public class Warrior extends Character implements Attacker {
+
+    private int stamina;
+    private int strength;
+
+    public Warrior(String name, int hp, int stamina, int strength) {
+        super(name, hp);
+        setStamina(stamina);
+        setStrength(strength);
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public void attack(Character character) {
+
+    }
 }
