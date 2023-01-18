452544b577cd8cab7bf0b3911ce8284541b07fb5
jewlys <jewlys@hotmail.com>
2023-01-18 17:45:40 +0100

    battle random on

---
+++ src/BattleManager.java
@@ -0,0 +1,13 @@
+public class BattleManager {
+    public static void createPlayers() {
+        //método para crear los jugadores, donde se pediría al usuario que ingrese el nombre de los jugadores y se crearían los objetos de Wizard o Warrior de acuerdo a lo que elijan
+    }
+
+    public static void chooseEnemies() {
+        //método para que el usuario escoja a su enemigo, en el cual se pediría al usuario que ingrese el nombre del enemigo y se buscaría en el arreglo de jugadores creados anteriormente
+    }
+
+    public static void startBattle() {
+        //método para iniciar la batalla, en el cual se ejecutarían los ataques y se irían actualizando los puntos de vida de los jugadores hasta que uno de ellos muera
+    }
+}
