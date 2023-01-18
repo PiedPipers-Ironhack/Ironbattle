4fb3d4e0a4838bd5d980543000b09756ee5300ff
manuelsherrera <manuelsherrera@gmail.com>
2023-01-18 18:30:28 +0100

    add: complete attack, given two parameters

--- src/Battle.java
+++ src/Battle.java
@@ -5,26 +5,14 @@ public class Battle {
     public static void battleStart(Wizard attacker, Wizard defense) throws IOException {
 
         FileWriter writer = new FileWriter("battlelog.txt", false);
-        //System.out.println("Intelligence: " + defense.getIntelligence());
-        //System.out.println("initial hp defense: " + attacker.getHp());
-        //System.out.println("initial hp attacker: " + test.getHp());
-        //System.out.println("attacker strength: " + test.getStrength());
-        //System.out.println("attacker stamina: " + test.getStamina());
-        //System.out.println("attacker mana: " + test2.getMana());
-        //test2.attack(test);
         while (defense.isAlive() && attacker.isAlive()) {
-
-
-
             writer.write((attacker.toString()) + "\n");
             writer.write(defense.toString() + "\n");
             writer.write(attacker.attack(defense) + "\n");
 
-
             writer.write((defense.toString()) + "\n");
             writer.write(attacker.toString() + "\n");
             writer.write(defense.attack(attacker) + "\n");
-
 
             if(defense.getHp() <= 0){
                 defense.setAlive(false);
@@ -40,29 +28,16 @@ public class Battle {
             }
         }
         writer.close();
-        //System.out.println("attacker remaining mana: " + test2.getMana());
-        //System.out.println("attacker remaining stamina: " + test.getStamina());
-        //System.out.println("remaining hp defense: " + test.getHp());
-
 
     }
     public static void battleStart(Wizard attacker, Warrior defense) throws IOException {
 
         FileWriter writer = new FileWriter("battlelog.txt", false);
-        //System.out.println("Intelligence: " + defense.getIntelligence());
-        //System.out.println("initial hp defense: " + attacker.getHp());
-        //System.out.println("initial hp attacker: " + test.getHp());
-        //System.out.println("attacker strength: " + test.getStrength());
-        //System.out.println("attacker stamina: " + test.getStamina());
-        //System.out.println("attacker mana: " + test2.getMana());
         while (defense.isAlive() && attacker.isAlive()) {
-
-
 
             writer.write((attacker.toString()) + "\n");
             writer.write(defense.toString() + "\n");
             writer.write(attacker.attack(defense) + "\n");
-
 
             writer.write((defense.toString()) + "\n");
             writer.write(attacker.toString() + "\n");
@@ -83,34 +58,21 @@ public class Battle {
             }
         }
         writer.close();
-        //System.out.println("attacker remaining mana: " + test2.getMana());
-        //System.out.println("attacker remaining stamina: " + test.getStamina());
-        //System.out.println("remaining hp defense: " + test.getHp());
-
 
     }
     public static void battleStart(Warrior attacker, Wizard defense) throws IOException {
 
         FileWriter writer = new FileWriter("battlelog.txt", false);
-        //System.out.println("Intelligence: " + defense.getIntelligence());
-        //System.out.println("initial hp defense: " + attacker.getHp());
-        //System.out.println("initial hp attacker: " + test.getHp());
-        //System.out.println("attacker strength: " + test.getStrength());
-        //System.out.println("attacker stamina: " + test.getStamina());
-        //System.out.println("attacker mana: " + test2.getMana());
+
         while (defense.isAlive() && attacker.isAlive()) {
-
-
 
             writer.write((attacker.toString()) + "\n");
             writer.write(defense.toString() + "\n");
             writer.write(attacker.attack(defense) + "\n");
 
-
             writer.write((defense.toString()) + "\n");
             writer.write(attacker.toString() + "\n");
             writer.write(defense.attack(attacker) + "\n");
-
 
             if(defense.getHp() <= 0){
                 defense.setAlive(false);
@@ -126,36 +88,22 @@ public class Battle {
             }
         }
         writer.close();
-        //System.out.println("attacker remaining mana: " + test2.getMana());
-        //System.out.println("attacker remaining stamina: " + test.getStamina());
-        //System.out.println("remaining hp defense: " + test.getHp());
-
 
     }
     public static void battleStart(Warrior attacker, Warrior defense) throws IOException {
 
 
             FileWriter writer = new FileWriter("battlelog.txt", false);
-            //System.out.println("Intelligence: " + defense.getIntelligence());
-            //System.out.println("initial hp defense: " + attacker.getHp());
-            //System.out.println("initial hp attacker: " + test.getHp());
-            //System.out.println("attacker strength: " + test.getStrength());
-            //System.out.println("attacker stamina: " + test.getStamina());
-            //System.out.println("attacker mana: " + test2.getMana());
 
             while (defense.isAlive() && attacker.isAlive()) {
-
-
 
                     writer.write((attacker.toString()) + "\n");
                     writer.write(defense.toString() + "\n");
                     writer.write(attacker.attack(defense) + "\n");
 
-
                     writer.write((defense.toString()) + "\n");
                     writer.write(attacker.toString() + "\n");
                     writer.write(defense.attack(attacker) + "\n");
-
 
                 if(defense.getHp() <= 0){
                     defense.setAlive(false);
@@ -171,10 +119,7 @@ public class Battle {
                 }
             }
 
-
         writer.close();
-            //System.out.println("attacker remaining mana: " + test2.getMana());
-            //System.out.println("attacker remaining stamina: " + test.getStamina());
-            //System.out.println("remaining hp defense: " + test.getHp());
+
     }
 }
